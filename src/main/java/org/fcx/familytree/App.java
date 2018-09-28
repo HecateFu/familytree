f371936de80415f9752eef8f2412e4a35ccc7c5d
Hecate Fu <hecatefu@outlook.com>
2018-09-29 00:55:36 +0800

    数据源和程序入口

--- src/main/java/org/fcx/familytree/App.java
+++ src/main/java/org/fcx/familytree/App.java
@@ -1,13 +1,17 @@
 package org.fcx.familytree;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Hello world!
+ * 程序入口
  *
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	System.setProperty("spring.profiles.active", "dev");
+        SpringApplication.run(App.class, args);
     }
 }
